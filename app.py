bac676e01f4c4defe1be01b468bccd1296316812
tarkptel <tspatel462@gmail.com>
2025-11-22 16:54:46 +0530

    simple_test

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
-print ("Its Tark Patel !!")
-print ("Its Devops eng")
-
-def test_add():
-    assert 1+1 == 2
-
-def test_massage():
-    assert "hello".upper()=='Hello'
+def test_dummy():
+    assert True
